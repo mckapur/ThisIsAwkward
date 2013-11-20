9504c2588c1a9b7a3dedae49d9f814005418c28b
MCKapur <me@rohankapur.com>
2013-11-20 10:28:20 +0800

    Added source code

---
+++ app.js
@@ -0,0 +1,108 @@
+var http = require('http');
+var express = require('express');
+var hbs = require('hbs');
+var twilio = require('twilio')
+var twilioClient = twilio('YOUR_ACCOUNT_SID', 'YOUR_AUTH_TOKEN');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use(express.bodyParser());
+app.use(express.static('views'));
+app.use(app.router);
+
+app.listen(3001);
+
+var message;
+var name = "Anonymous";
+var phoneNumber;
+
+app.post('/go', function(req, res) {
+
+	var status = "We fired your call!";
+
+	if (req.body.callMessageToRecipient) {
+
+		message = req.body.callMessageToRecipient;
+	}
+
+	if (req.body.callSenderName) {
+
+		name = req.body.callSenderName;
+	}
+
+	if (req.body.callRecipientPhoneNumber) {
+
+		phoneNumber = req.body.callRecipientPhoneNumber;
+	}
+
+	if (message && phoneNumber) {
+
+		twilioClient.makeCall ({
+
+			to: phoneNumber,
+			from: '+13478481674',
+			url: 'http://thisisawkward.rohankapur.com/voice'
+		},
+		function(err, responseData) {
+
+		    if (err) {
+
+		    	status = err;
+
+		    	updateStatusWithResponse(1, status, res);
+		    }
+		    else {
+
+		    	updateStatusWithResponse(0, status, res);
+		    }
+		});
+	}
+	else {
+
+		status = "Please fill in all fields!";
+
+		updateStatusWithResponse(1, status, res);
+	}
+});
+
+app.post('/voice', function(req, res) {
+
+	var twiml = new twilio.TwimlResponse();
+
+    twiml.say("The following message has been sent from: " + name);
+    twiml.pause({length: 3});		        
+    twiml.say(message);
+    twiml.pause({length: 3});	
+    twiml.hangup();
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+function updateStatusWithResponse(isError, customStatus, res) {
+
+	var err = '';
+
+	if (isError == 1) {
+
+		err = 'Could not make call: '
+	}
+
+	res.render('index', {
+
+		response: {
+
+			status: err + customStatus
+		}
+	});
+}
+
+app.get('/', function(req, res) {
+
+	res.render('index', {
+
+	});
+});
